eb10aaa000d93bd04f973215da6fcd8a96cc30ae
zuowendong <17326195691@163.com>
2023-05-17 17:15:38 +0800

    chore: update readme

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# 可视化大屏设计器
+<p align="center">
+  <font Size=6>可视化大屏设计器</font>
+</p>
+<p align="center">
+  <font Size=5>Visual Designer</font>
+</p>
+
+## :eyes: 预览
+
+<!-- <https://zuowendong.github.io/visual-designer/> -->
+
+最新版本
+
+:arrow_right: <https://zuowendong.github.io/visual-designer/>
+
+服务器部署，版本滞后
+
+:arrow_right: <http://106.53.143.75:8080/>
+
+## :tada: 开发
+
+使用`pnpm`安装依赖
+
+```shell
+pnpm install
+```
+
+构建可视化组件库
+
+```shell
+pnpm build:ui
+```
+
+运行
+
+```shell
+pnpm dev
+```
--- components.d.ts
+++ components.d.ts
@@ -15,7 +15,7 @@ declare module '@vue/runtime-core' {
     ComponentData: typeof import('./src/components/sidebar-component/ComponentData.vue')['default']
     ComponentEvent: typeof import('./src/components/sidebar-component/ComponentEvent.vue')['default']
     ComponentProperty: typeof import('./src/components/sidebar-component/ComponentProperty.vue')['default']
-    NavigationView: typeof import('./src/components/navigation/NavigationView.vue')['default']
+    NavigationView: typeof import('./src/components/Navigation/NavigationView.vue')['default']
     NColorPicker: typeof import('naive-ui')['NColorPicker']
     NConfigProvider: typeof import('naive-ui')['NConfigProvider']
     NInput: typeof import('naive-ui')['NInput']
@@ -28,8 +28,8 @@ declare module '@vue/runtime-core' {
     ShapeBox: typeof import('./src/components/canvas-box/ShapeBox.vue')['default']
     SidebarComponent: typeof import('./src/components/sidebar-component/SidebarComponent.vue')['default']
     SidebarList: typeof import('./src/components/sidebar-list/SidebarList.vue')['default']
-    SwitchLanguage: typeof import('./src/components/navigation/SwitchLanguage.vue')['default']
-    ThemeButton: typeof import('./src/components/navigation/ThemeButton.vue')['default']
+    SwitchLanguage: typeof import('./src/components/Navigation/SwitchLanguage.vue')['default']
+    ThemeButton: typeof import('./src/components/Navigation/ThemeButton.vue')['default']
     VirtualList: typeof import('./src/components/sidebar-list/VirtualList.vue')['default']
   }
 }
